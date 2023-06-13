a30b381317a93a7c130738ac8e3ff17717b6cc3a
Iñaki Pujol <ipujol10@gmail.com>
2023-06-13 13:44:23 +0200

    ADD: starting point

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+package_name = "sensor_bands_gui"
+
+setup(
+		name=package_name,
+		version="0.0.0",	
+		packages=[package_name],
+		data_files=[
+				("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+		],
+		install_requires=["setuptools"],
+    zip_safe=True,
+		maintainer="inaki",
+		maintainer_email="ipujol10@gmail.com",
+		description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest", "pynput"],
+		entry_points={
+				"console_scripts": ["prova = sensor_bands_gui.prova:main"],
+		},
+)
